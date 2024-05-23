3bd85e86ae1dc3c79d935dbb3455f3e2df49d7f5
Conor Howard <47044266+conorhoward6510@users.noreply.github.com>
2024-05-23 09:59:14 +0100

    Added functionality to show/hide the share fact for through a css class

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello, world!");
+const btn = document.querySelector(".btn-open");
+const form = document.querySelector("#fact-form");
+btn.addEventListener("click", function () {
+  if (form.classList.contains("hidden")) {
+    form.classList.remove("hidden");
+    btn.innerHTML = "Close";
+  } else {
+    form.classList.add("hidden");
+    btn.innerHTML = "Share a fact";
+  }
+});
