ba0282bd92b3681817050d8689d519b821124315
Conor Howard <47044266+conorhoward6510@users.noreply.github.com>
2024-06-01 23:09:44 +0100

    Addes facts-list class and used JS to clear the list

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const btn = document.querySelector(".btn-open");
 const form = document.querySelector("#fact-form");
+const factsList = document.querySelector(".facts-list");
+
 btn.addEventListener("click", function () {
   if (form.classList.contains("hidden")) {
     form.classList.remove("hidden");
@@ -9,3 +11,5 @@ btn.addEventListener("click", function () {
     btn.innerHTML = "Share a fact";
   }
 });
+
+factsList.innerHTML = "";
