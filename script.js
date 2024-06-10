65a0ab04f8f83c8220aa975de110411d3300ed53
Conor Howard <47044266+conorhoward6510@users.noreply.github.com>
2024-06-10 22:02:01 +0100

    Overhauled all files to bring it back in line with the instructors files after I strayed from his methods and tried things my own way

--- script.js
+++ script.js
@@ -1,40 +1,58 @@
-// Initial Test Data
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
 
-// const initialFacts = [
-//   {
-//     id: 1,
-//     text: "React is being developed by Meta (formerly facebook)",
-//     source: "https://opensource.fb.com/",
-//     category: "technology",
-//     votesInteresting: 24,
-//     votesMindblowing: 9,
-//     votesFalse: 4,
-//     createdIn: 2021,
-//   },
-//   {
-//     id: 2,
-//     text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
-//     source:
-//       "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
-//     category: "society",
-//     votesInteresting: 11,
-//     votesMindblowing: 2,
-//     votesFalse: 0,
-//     createdIn: 2019,
-//   },
-//   {
-//     id: 3,
-//     text: "Lisbon is the capital of Portugal",
-//     source: "https://en.wikipedia.org/wiki/Lisbon",
-//     category: "society",
-//     votesInteresting: 8,
-//     votesMindblowing: 3,
-//     votesFalse: 1,
-//     createdIn: 2015,
-//   },
-// ];
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
 
-// Load Data From Supabase
+// Selecting DOM elements
+const btn = document.querySelector(".btn-open");
+const form = document.querySelector(".fact-form");
+const factsList = document.querySelector(".facts-list");
+
+// Create DOM elements: Render facts in list
+factsList.innerHTML = "";
+
+// Load data from Supabase
+loadFacts();
 
 async function loadFacts() {
   const res = await fetch(
@@ -49,57 +67,42 @@ async function loadFacts() {
     }
   );
   const data = await res.json();
+
   createFactsList(data);
 }
 
-// Functions
-
 function createFactsList(dataArray) {
   const htmlArr = dataArray.map(
-    (fact) => `
-      <li class="fact">
-        <p>
-          ${fact.text}
-          <a
-            class="source"
-            href=${fact.source}
-            target="_blank"
-            >Source</a
-          >
-        </p>
-        <span class="tag ${fact.category}-tag">${fact.category}</span>
-        <div class="vote-buttons">
-          <button>👍 ${fact.votesInteresting}</button>
-          <button>🤯 ${fact.votesMindblowing}</button>
-          <button>⛔️ ${fact.votesFalse}</button>
-        </div>
-      </li>
-    `
+    (fact) => `<li class="fact">
+    <p>
+    ${fact.text}
+      <a
+        class="source"
+        href="${fact.source}"
+        target="_blank"
+      >(Source)</a>
+    </p>
+    <span class="tag" style="background-color: ${
+      CATEGORIES.find((cat) => cat.name === fact.category).color
+    }">${fact.category}</span>
+    <div class="vote-buttons">
+      <button>👍 ${fact.votesInteresting}</button>
+      <button>🤯 ${fact.votesMindblowing}</button>
+      <button>⛔️ ${fact.votesFalse}</button>
+    </div>
+  </li>`
   );
   const html = htmlArr.join("");
   factsList.insertAdjacentHTML("afterbegin", html);
 }
 
-// Selecting DOM Elements
-
-const btn = document.querySelector(".btn-open");
-const form = document.querySelector("#fact-form");
-const factsList = document.querySelector(".facts-list");
-
-// Create DOM Elements: Render facts in list
-
-factsList.innerHTML = "";
-// createFactsList(initialFacts);
-loadFacts();
-
-// Toggle Form Visibility
-
+// Toggle form visibility
 btn.addEventListener("click", function () {
   if (form.classList.contains("hidden")) {
     form.classList.remove("hidden");
-    btn.innerHTML = "Close";
+    btn.textContent = "Close";
   } else {
     form.classList.add("hidden");
-    btn.innerHTML = "Share a fact";
+    btn.textContent = "Share a fact";
   }
 });
